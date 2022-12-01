e94916abb7c181abf77f63cf890f8c4f39a4bbcc
Alejandro <webmaster@MacBook-Pro-de-Alejandro.local>
2022-12-01 14:55:16 -0600

    config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ReCaptchaV3Service } from 'ng-recaptcha';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'app-14';
+  recaptchaToken: string = '';
+
+  constructor(private recaptcha: ReCaptchaV3Service) {}
+
+  copyClipboard(): void {
+    const element = document.querySelector('#token') as HTMLElement;
+    navigator.clipboard.writeText(element.textContent as string);
+  }
+
+  async generateRecaptcha(): Promise<void> {
+    const tokenRecaptcha = this.recaptcha.execute('/');
+    this.recaptchaToken = await lastValueFrom(tokenRecaptcha);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RecaptchaV3Module, RECAPTCHA_V3_SITE_KEY } from 'ng-recaptcha';
+
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, RecaptchaV3Module],
+  providers: [
+    {
+      provide: RECAPTCHA_V3_SITE_KEY,
+      useValue: '',
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
